a070e5063201893c570b7babe4a234ec4bac6ffc
Taeyooooon <eric568@naver.com>
2023-07-04 17:57:41 +0900

    Fix: darkmode button

--- src/components/ui/DarkModeButton.tsx
+++ src/components/ui/DarkModeButton.tsx
@@ -2,13 +2,18 @@
 
 import React from 'react';
 import { useDarkMode } from '@/context/DarkModeContext';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 
 const DarkModeButton = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <button onClick={toggleDarkMode} className='btn btn-outline fixed bottom-20 right-20'>
-      {darkMode ? '다크' : '라이트'}
+    <button className='btn btn-circle btn-ghost bg-backGround bg-opacity-90 fixed top-4 right-4 '>
+      <label className='swap swap-rotate w-12 h-12'>
+        <input type='checkbox' onChange={toggleDarkMode} checked={darkMode} />
+        <BsFillSunFill className=' text-xl text-white swap-on' />
+        <BsFillMoonFill className='text-xl text-white swap-off' />
+      </label>
     </button>
   );
 };
--- src/components/ui/ScrollTopButton.tsx
+++ src/components/ui/ScrollTopButton.tsx
@@ -30,7 +30,7 @@ const ScrollTopButton = () => {
   };
 
   return (
-    <button className=' btn-circle btn fixed bottom-20 right-4 bg-white z-20'>
+    <button className='btn btn-circle dark:bg-white border-backGround fixed bottom-20 right-4 z-20'>
       {isScrollable ? (
         <Lottie options={lottieOptions} ariaRole='img' isClickToPauseDisabled />
       ) : (
